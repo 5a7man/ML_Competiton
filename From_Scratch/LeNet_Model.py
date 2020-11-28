889566efaa326d7b520bc462dbcaf110519c09f4
Muhammad Salman Kabir <58083322+5a7man@users.noreply.github.com>
2020-11-29 00:26:27 +0500

    LeNet Model from Scratch, Acc: 0.7474

--- From_Scratch/LeNet_Model.py
+++ From_Scratch/LeNet_Model.py
@@ -7,10 +7,20 @@ Original file is located at
     https://colab.research.google.com/drive/1hb5fbap_RjGgzdVi3HyXFxRSNI0O8G57
 """
 
-from google.colab import drive
-drive.mount('/content/gdrive')
+seed_value=42
+
+import os
+os.environ['PYTHONHASHSEED']=str(seed_value)
+
+import random
+random.seed(seed_value)
 
 import numpy as np
+np.random.seed(seed_value)
+
+import tensorflow as tf
+tf.random.set_seed(seed_value)
+
 from tensorflow.keras.layers import Conv2D, MaxPool2D, Flatten, Dense, Dropout, BatchNormalization
 from tensorflow.keras import Sequential
 from tensorflow.keras.optimizers import Adam
@@ -20,6 +30,7 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 # Data Importing
 x_train = np.load('/content/drive/My Drive/Compition/xs.npy')
 y_train = np.load('/content/drive/My Drive/Compition/ys.npy')
+x_train = x_train/255.0
 
 def build_model():
   m = Sequential()
@@ -33,10 +44,10 @@ def build_model():
   m.add(Flatten())
   m.add(Dense(64,activation='relu'))
   m.add(BatchNormalization())
-  m.add(Dropout(0.3))
+  m.add(Dropout(0.1))
   m.add(Dense(32,activation='relu'))
   m.add(BatchNormalization())
-  m.add(Dropout(0.3))
+  m.add(Dropout(0.1))
   m.add(Dense(9,activation='softmax'))
   return m
 
@@ -45,13 +56,13 @@ model = build_model()
 early_stopping = EarlyStopping(monitor = 'val_accuracy', patience =  10, restore_best_weights = True)
 lr_sechudle = ReduceLROnPlateau(monitor = 'val_accuracy', factor = 0.2, patience =  6 )
 
-model.compile(optimizer=Adam(1e-5),
+model.compile(optimizer=Adam(1e-3),
               loss = 'sparse_categorical_crossentropy',
               metrics = ['accuracy'])
 
 model.summary()
 
-generator = ImageDataGenerator(rotation_range=90,
+generator = ImageDataGenerator(rotation_range=10,
                                zoom_range = 0.2,
                                horizontal_flip = True,
                                vertical_flip = True,
@@ -62,8 +73,8 @@ generator = ImageDataGenerator(rotation_range=90,
 model.fit(generator.flow(x_train,y_train,batch_size = 25),
           validation_data = generator.flow(x_train,y_train,batch_size = 25, subset = 'validation'),
           steps_per_epoch = len(x_train)/25,
-          epochs = 60,
+          epochs = 100,
           verbose =  2,
           callbacks=[early_stopping, lr_sechudle])
 
-model.save('lenet_model_3.h5')
+model.save('model.h5')
